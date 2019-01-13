91e914fb9098db578a26d656399acb9f6fc0626f
Ruslan Merzlyakov <ruslankko@gmail.com>
2019-01-13 18:40:57 +0500

    add overlay menu

--- src/assets/scripts/modules/hamburger.js
+++ src/assets/scripts/modules/hamburger.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const hamburger = document.querySelector('.hamburger');
+  const overlay = document.querySelector('.js-overlay-menu');
   hamburger.onclick = function (e) {
     e.preventDefault();
+
     this.classList.toggle('on');
+    overlay.classList.toggle('open');
   };
 
 });
