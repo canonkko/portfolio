3fb60b614e355012f7aefcc456650be2a13cdf96
Ruslan Merzlyakov <ruslankko@gmail.com>
2019-01-16 08:04:27 +0500

    Правки стилей

--- src/assets/scripts/modules/flip.js
+++ src/assets/scripts/modules/flip.js
@@ -4,21 +4,34 @@ document.addEventListener('DOMContentLoaded', function () {
   const flipbtnMain = document.querySelector('.js-user-links__link');
   const flipCart = document.querySelector('.hero__cart');
 
-  flipbtn.onclick = function (e) {
-    e.preventDefault();
-    if (flipCart.classList.contains('flipped')) {
-      flipCart.classList.remove('flipped');
-      this.innerHTML = 'Авторизоваться'; 
-    } else {
-      flipCart.classList.add('flipped');
-      this.innerHTML = 'На главную';
+  // flipbtn.onclick = function (e) {
+  //   e.preventDefault();
+  //   if (flipCart.classList.contains('flipped')) {
+  //     flipCart.classList.remove('flipped');
+  //     this.innerHTML = 'Авторизоваться'; 
+  //   } else {
+  //     flipCart.classList.add('flipped');
+  //     this.innerHTML = 'На главную';
+  //   }
+  // }
+
+  // flipbtnMain.onclick = function (e) {
+  //   e.preventDefault();
+  //   flipCart.classList.remove('flipped');
+  //   flipbtn.innerHTML = 'Авторизоваться';
+  // }
+
+  document.body.onclick = (e) => {
+    
+    // e.preventDefault();
+    if (!e.target.closest('.hero__cart')) {
+      // console.log("fgf");
+      if (!flipCart.classList.contains('flipped') && e.target !== flipbtnMain) {
+        return false;
+        flipCart.classList.toggle('flipped');
+        flipbtn.innerHTML = 'Авторизоваться';
+      }
     }
   }
 
-  flipbtnMain.onclick = function (e) {
-    e.preventDefault();
-    flipCart.classList.remove('flipped');
-    flipbtn.innerHTML = 'Авторизоваться';
-  }
-
-})
+});
